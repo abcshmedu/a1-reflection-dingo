9a8f7714e59fee3277bd817fcdbd2f2de44f5634
Faaux <c.brammer1@gmail.com>
2017-03-29 13:54:34 +0200

    New POM

---
+++ src/test/java/Tester.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by cbram on 29.03.2017.
+ */
+public class Tester
+{
+    @Test()
+    public void SomeTest()
+    {
+        Assert.assertEquals(0,0);
+    }
+}
